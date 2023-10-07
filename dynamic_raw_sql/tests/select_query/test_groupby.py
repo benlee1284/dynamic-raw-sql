1422428355f65a71302a26f76eac64c2707f078e
benlee1284 <ben.lee@kayenta.io>
2023-10-07 16:26:06 +0100

    Add test for adding group by clauses to empty query

--- dynamic_raw_sql/tests/select_query/test_groupby.py
+++ dynamic_raw_sql/tests/select_query/test_groupby.py
@@ -24,6 +24,23 @@ def test_instantiate_query_with_groupby_clause(
 
 
 @pytest.mark.parametrize(
+    "group_by_clauses, expected_query",
+    [
+        pytest.param(["column_1"], "SELECT GROUP BY column_1", id="singular_clause"),
+        pytest.param(
+            ["column_1", "1"], "SELECT GROUP BY column_1, 1", id="multiple_clauses"
+        ),
+        pytest.param(["column_1", 1], "SELECT GROUP BY column_1, 1", id="mixed_types"),
+    ],
+)
+def test_add_group_by_clauses_to_empty_query(
+    group_by_clauses: Iterable[Any], expected_query: str
+) -> None:
+    query = SelectQuery().group_by(*group_by_clauses)
+    assert query.build() == expected_query
+
+
+@pytest.mark.parametrize(
     "group_by_clauses",
     [
         pytest.param(1, id="int"),
