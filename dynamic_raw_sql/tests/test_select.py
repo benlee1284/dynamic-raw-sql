2cf1aca9e67a943ad5ea0aa709cae332184bd58c
benlee1284 <ben.lee@kayenta.io>
2023-10-07 14:46:50 +0100

    Test instantiating query with where conditions

--- dynamic_raw_sql/tests/test_select.py
+++ dynamic_raw_sql/tests/test_select.py
@@ -148,6 +148,32 @@ def test_add_where_clause_to_empty_query(conditions: list[str], expected_query: 
     assert query.build() == expected_query
 
 
+@pytest.mark.parametrize(
+    "conditions, expected_query",
+    [
+        pytest.param(
+            ["1=1"],
+            "SELECT  WHERE 1=1",
+            id="singular_condition"
+        ),
+        pytest.param(
+            ["1=1", "column_1=42"],
+            "SELECT  WHERE 1=1 AND column_1=42",
+            id="multiple_conditions"
+        ),
+        pytest.param(
+            ["column_1=%s"],
+            "SELECT  WHERE column_1=%s",
+            id="condition_with_parameter"
+        ),
+    ],
+)
+def test_instantiate_query_with_where_conditions(
+    conditions: list[str], expected_query: str
+) -> None:
+    query = SelectQuery(where_conditions=conditions)
+
+    assert query.build() == expected_query
 def test_instantiating_with_invalid_where_condition_raises_error() -> None:
     with pytest.raises(TypeError):
         SelectQuery(where_conditions="1=1")
