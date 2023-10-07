31ca99462c67c832df9cda1a6d6d91dec2fe880e
benlee1284 <ben.lee@kayenta.io>
2023-10-07 23:09:15 +0100

    lint

--- dynamic_raw_sql/tests/select_query/test_full_query.py
+++ dynamic_raw_sql/tests/select_query/test_full_query.py
@@ -10,19 +10,14 @@ def test_full_query() -> None:
             "SUM(column_3)",
             "1",
         ],
-        where_conditions=[
-            "1=1",
-            "column_1=42",
-            "column_2=144 OR column_3=20"
-        ],
+        where_conditions=["1=1", "column_1=42", "column_2=144 OR column_3=20"],
         group_by_elements=[
             "column_1",
             "column_2",
         ],
     )
     assert (
-        query.build()
-        == "SELECT column_1, column_2, SUM(column_3), 1 "
+        query.build() == "SELECT column_1, column_2, SUM(column_3), 1 "
         "FROM table WHERE 1=1 AND column_1=42 AND column_2=144 OR column_3=20 "
         "GROUP BY column_1, column_2"
     )
