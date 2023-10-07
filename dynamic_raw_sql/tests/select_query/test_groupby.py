ea2c9f4cd1626bb11587d38017ec92e90917a3c0
benlee1284 <ben.lee@kayenta.io>
2023-10-07 15:41:10 +0100

    Add test for initialising with groupby

--- dynamic_raw_sql/tests/select_query/test_groupby.py
+++ dynamic_raw_sql/tests/select_query/test_groupby.py
@@ -1,4 +1,22 @@
 from collections.abc import Iterable
 import pytest
+from typing import Any
 
 from dynamic_raw_sql import SelectQuery
+
+
+@pytest.mark.parametrize(
+    "group_by_clauses, expected_query",
+    [
+        pytest.param(["column_1"], "SELECT GROUP BY column_1", id="singular_clause"),
+        pytest.param(
+            ["column_1", "1"], "SELECT GROUP BY column_1, 1", id="multiple_clauses"
+        ),
+        pytest.param(["column_1", 1], "SELECT GROUP BY column_1, 1", id="mixed_types"),
+    ],
+)
+def test_instantiate_query_with_groupby_clause(
+    group_by_clauses: Iterable[Any], expected_query: str
+) -> None:
+    query = SelectQuery(group_by=group_by_clauses)
+    assert query.build() == expected_query
