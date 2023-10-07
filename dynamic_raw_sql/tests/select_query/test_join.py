2114c286ff23dd20d27596913ea511ac13a07aaf
benlee1284 <ben.lee@kayenta.io>
2023-10-07 21:23:50 +0100

    Add tests for invalid joins

--- dynamic_raw_sql/tests/select_query/test_join.py
+++ dynamic_raw_sql/tests/select_query/test_join.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 from dynamic_raw_sql import SelectQuery
 
@@ -13,3 +14,16 @@ def test_instantiate_query_with_join_clause() -> None:
         query.build() == "SELECT  INNER JOIN table2 ON 1=1 "
         "LEFT OUTER JOIN table3 ON column_1=column_2"
     )
+
+
+@pytest.mark.parametrize(
+    "joins",
+    [
+        pytest.param("INNER JOIN table2 ON 1=1", id="string"),
+        pytest.param(1, id="int"),
+        pytest.param([1, 2], id="list of ints"),
+    ],
+)
+def test_instantiating_query_with_invalid_join_clause_erros(joins: Any) -> None:
+    with pytest.raises(TypeError):
+        SelectQuery(joins=joins)
