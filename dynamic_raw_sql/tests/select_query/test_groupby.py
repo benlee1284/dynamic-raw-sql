c2aa456a49952da624268319a341519b7155b8e1
benlee1284 <ben.lee@kayenta.io>
2023-10-07 15:54:46 +0100

    Add test for raising error on instantiation with non-iterable groupby

--- dynamic_raw_sql/tests/select_query/test_groupby.py
+++ dynamic_raw_sql/tests/select_query/test_groupby.py
@@ -21,3 +21,18 @@ def test_instantiate_query_with_groupby_clause(
 ) -> None:
     query = SelectQuery(group_by_elements=group_by_clauses)
     assert query.build() == expected_query
+
+
+@pytest.mark.parametrize(
+    "group_by_clauses",
+    [
+        pytest.param(1, id="int"),
+        pytest.param("1", id="str"),
+        pytest.param(3.14, id="float"),
+    ],
+)
+def test_instantiating_query_with_non_iterable_groupby_clause_raises_error(
+    group_by_clauses: Any,
+) -> None:
+    with pytest.raises(TypeError):
+        SelectQuery(group_by_elements=group_by_clauses)
