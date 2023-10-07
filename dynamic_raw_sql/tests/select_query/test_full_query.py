9f6fc405117c2bdcaa518f46cc37f6467c7f8206
benlee1284 <ben.lee@kayenta.io>
2023-10-07 23:10:33 +0100

    Include join on full query test

--- dynamic_raw_sql/tests/select_query/test_full_query.py
+++ dynamic_raw_sql/tests/select_query/test_full_query.py
@@ -10,6 +10,7 @@ def test_full_query() -> None:
             "SUM(column_3)",
             "1",
         ],
+        joins=["INNER JOIN table2 ON 1=1", "LEFT OUTER JOIN table3 ON column_1=column_2"],
         where_conditions=["1=1", "column_1=42", "column_2=144 OR column_3=20"],
         group_by_elements=[
             "column_1",
@@ -18,6 +19,9 @@ def test_full_query() -> None:
     )
     assert (
         query.build() == "SELECT column_1, column_2, SUM(column_3), 1 "
-        "FROM table WHERE 1=1 AND column_1=42 AND column_2=144 OR column_3=20 "
+        "FROM table "
+        "INNER JOIN table2 ON 1=1 "
+        "LEFT OUTER JOIN table3 ON column_1=column_2 "
+        "WHERE 1=1 AND column_1=42 AND column_2=144 OR column_3=20 "
         "GROUP BY column_1, column_2"
     )
