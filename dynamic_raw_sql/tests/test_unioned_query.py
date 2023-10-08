de264d13bb796b4d2dbeb6f1c432ccc82602b36a
benlee1284 <ben.lee@kayenta.io>
2023-10-08 15:04:39 +0100

    Add test for union all

--- dynamic_raw_sql/tests/test_unioned_query.py
+++ dynamic_raw_sql/tests/test_unioned_query.py
@@ -15,3 +15,20 @@ def test_instantiating_unioned_query_with_select_queries_produces_valid_query() 
         ]
     )
     assert query.build() == "(SELECT * FROM table) UNION (SELECT * FROM table)"
+
+
+def test_instantiating_union_all_query() -> None:
+    query = UnionedQuery(
+        queries=[
+            SelectQuery(
+                from_table="table",
+                select_elements=["*"]
+            ),
+            SelectQuery(
+                from_table="table",
+                select_elements=["*"]
+            ),
+        ],
+        union_all=True,
+    )
+    assert query.build() == "(SELECT * FROM table) UNION ALL (SELECT * FROM table)"
