57c3703fd192c498d21f98aab37c4974e0203c8a
benlee1284 <ben.lee@kayenta.io>
2023-10-08 15:06:16 +0100

    Satisfy test

--- dynamic_raw_sql/unioned_query.py
+++ dynamic_raw_sql/unioned_query.py
@@ -7,14 +7,23 @@ if TYPE_CHECKING:
 
 
 class UnionedQuery:
-    __slots__ = ("__queries",)
+    __slots__ = (
+        "__queries",
+        "__union_all",
+    )
 
     def __init__(
         self,
         queries: Iterable["SelectQuery"],
+        union_all: bool = False,
     ) -> None:
         self.__queries = queries
+        self.__union_all = union_all
 
     def build(self) -> str:
-        query_body = ") UNION (".join(query.build() for query in self.__queries)
+        union_type = "UNION ALL" if self.__union_all else "UNION"
+
+        query_body = f") {union_type} (".join(
+            query.build() for query in self.__queries
+        )
         return f"({query_body})"
